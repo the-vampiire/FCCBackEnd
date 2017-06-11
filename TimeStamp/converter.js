980e246d28ce2dcbc7eef4556e9004afa2e85f8d
Patrick Cleary <vampiirecodes@gmail.com>
2017-06-11 17:31:24 -0400

    server works and parses the dates passed as a req paramaeter. accepts unix or long form dates, returns both converted properly * up next: build the index.html front end

--- TimeStamp/converter.js
+++ TimeStamp/converter.js
@@ -1,3 +1,26 @@
 /**
  * Created by Vampiire on 6/11/17.
  */
+
+
+function convertTime(time){
+
+    // console.log(`long date: ${longDate}`);
+
+    if(~time.indexOf(' ')){
+        return {unix: Date.parse(time)/1000, long: time};
+    }
+
+    else{
+        console.log('else called');
+        time = Number(time)*1000;
+        let longDate = new Date(time).toDateString();
+        return {unix : time/1000, long: longDate.slice(longDate.indexOf(' ')+1)};
+    }
+
+
+}
+
+module.exports = {
+  time : convertTime
+};
